e2146f2d6841f9760654b7367ee68eb0426e5baa
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2022-02-16 07:12:57 +0300

    Merge branch 'isotope'

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,37 @@ import 'lazysizes';
 // global alert
 import './assets/js/alert';
 
+import { sortboard } from './node_modules/sortboard/dist/sortboard.umd.min.js';
 
+const sb = sortboard({
+    container: '#sortlist',
+    selector: '.card',
+    gutter: 15,
+})
+
+const anchors = children('#filters a')
+
+anchors.forEach(el => {
+    el.addEventListener(
+        'click',
+        event => {
+            const an = event.target
+
+            if (!an.classList.contains('active')) {
+                const data = an.getAttribute('data-filter')
+                sb.filter(data)
+
+                anchors.forEach(a => {
+                    a.classList.remove('active')
+                })
+                an.classList.add('active')
+            }
+        },
+        false
+    )
+})
+
+function children(selector, parent = document) {
+    return Array.prototype.slice
+        .call(parent.querySelectorAll(selector))
+}
