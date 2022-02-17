33fc93975c9f00a1879c4cab1ca826cf181d34d5
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2022-02-17 19:23:05 +0300

    update 7 files, create 2 files, move 2 files and delete 1 file

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,15 +55,3 @@ function displaybutton(down) {
     }
 }
 
-/* //Start of Tawk.to Script
-// eslint-disable-next-line no-unused-vars
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement('script'), s0 = document.getElementsByTagName('script')[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/620dd9f7a34c24564126b0bb/1fs31mrlc';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-//End of Tawk.to Script */
---
+++ content/utility/rarity.md
@@ -0,0 +1,8 @@
+---
+title: "Rarity"
+description: ""
+date: 2022-02-17T18:42:03+03:00
+lastmod: 2022-02-17T18:42:03+03:00
+draft: false
+images: []
+---
